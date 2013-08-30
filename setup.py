a08014aff0adab432a53c7694b88162ff1a88cc4
Alex Morozov <morozov@podmoskovie.info>
2013-08-30 12:52:27 +0400

    .

--- setup.py
+++ setup.py
@@ -19,5 +19,5 @@ setup(name='pyscrum',
               'mkburndown = pyscrum.tools.mkburndown:main',
           ]
       }
-      include_package_data: True,
+      include_package_data=True,
      )
